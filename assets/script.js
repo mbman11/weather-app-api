aa39e9d29b1ad13e6b2a21b7ba0ce8255f4dd497
mbman11 <m.behrmann11@gmail.com>
2023-08-03 16:37:01 -0600

    add js to display currrent weather, added js to display forecast, edited css to add style

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,152 @@
 var apiKey = "656a0c48d465cbcb32447829872dc37e";
 
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=5" + "&appid=" + apiKey;
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=38.2688&lon=-77.5476&per_page=5&" + userInput + "&units=imperial" + "&appid=" + apiKey;
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
 var fetchButton = document.getElementById('button');
 
 var userInput = document.getElementById('user-input').value;
 
 
 
-
 function textValue() {
   var userInput = document.getElementById('user-input').value;
-  // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=38.2688&lon=-77.5476&per_page=5&" + userInput + "&units=imperial" + "&appid=" + apiKey;
 
-  var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=5" + "&appid=" + apiKey;
+  var idLeftDiv = document.getElementById('left-side-div');
 
-  getApi(queryURL);
+  var createUserInEl = document.createElement('h4');
+  createUserInEl.textContent = userInput;
+
+  idLeftDiv.append(createUserInEl);
+
+
+  var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?zip=" + userInput + "&appid=" + apiKey + "&units=imperial";
+  var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?zip" + userInput + "&appid=" + apiKey + "&units=imperial";
+  currentWeather(queryURL2 , queryURL1)
+  
+  
 }
 
-
-
-function getApi(queryURL) {
-  fetch(queryURL).then(function(response){
+function currentWeather(queryURL2, queryURL1) {
+  fetch(queryURL1).then(function (response) {
     return response.json();
-  }).then(function(data){
-    console.log(data);
-    console.log(data[0].name);
-    console.log(data[0].state);
-    console.log(data[0].country);
-    // console.log("City: " + data.name);
-    // console.log("Current Temperature: " + data.main.temp);
-    // console.log(data.weather[0].description);
+  }).then(function (data) {
+
+    var  mainResultC = document.getElementById('result')
+    var cWeatherArea = document.getElementById('current-weather');
+    var cWeatherContent = data.city.name + ', ' + data.city.country + ' ' + data.list[0].dt_txt.split(" ")[0];
+
+    var temperature = document.createElement('p')
+    temperature.textContent = 'Temp: ' + data.list[0].main.temp + '°F';
+
+    var wind = document.createElement('p')
+    wind.textContent = 'Wind Speed: ' + data.list[0].wind.speed + ' MPH';
+
+    var humidity = document.createElement('p')
+    humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + ' MPH';
+
+
+    var cWeatherInfoEl = document.createElement('h2');
+    var img = document.createElement("img");
+    img.setAttribute("src",`https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`);
+
+    cWeatherInfoEl.textContent = cWeatherContent;
+
+ 
+    cWeatherArea.append(cWeatherInfoEl);
+    cWeatherArea.append(img);
+    cWeatherArea.append(temperature);
+    cWeatherArea.append(wind);
+    cWeatherArea.append(humidity);
+
+    mainResultC.append(cWeatherArea);
+
+
+
+    // console.log(data.list[0].dt_txt);
+    // today forecast can happen right here    
+
+    
+    // currentWeather(queryURL2);
+    getFiveDay(queryURL1);
   })
 }
 
-
-fetchButton.addEventListener('click',textValue);
-// fetchButton.addEventListener('click',getApi);
-
-
-
-
-
-
-
+function getFiveDay(queryURL) {
+  fetch(queryURL).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
+    handleApiResponse(data);
+  })
+};
 
 
 
 
 
 
+function handleApiResponse(data) {
+  var resultContainer = document.getElementById('five-day');
+  // Limit the number of results to 5 from the weather list
+  var weatherList = data.list
+  //reset container
+  resultContainer.innerHTML = "";
 
 
-// function getApi() {
-//   fetch(queryURL).then(function(response){
-//     return response.json()
-//   }).then(function(data){
-//     console.log(data.name);
-//     console.log(data.main.temp);
-//     console.log(data.weather[0].main);
-//     console.log(data.weather[0].description);
+  weatherList.forEach(function (item) {
+    var time = item.dt_txt.split(" ")[1]
+    if (time === "12:00:00") {
+      // Create a new div element for each weather forecast object
+      var weatherSection = document.createElement('div');
+      weatherSection.classList.add('weather-section'); // Add a CSS class for styling (optional)
+      var img = document.createElement("img");
+      img.setAttribute("src",`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`)
+
+      // Create and populate elements with the relevant weather forecast data
+      var cityName = document.createElement('h3')
+      cityName.textContent = data.city.name;
+
+      var date = document.createElement('p');
+      date.textContent = "Date: " + item.dt_txt.split(" ")[0];
+
+      var temperature = document.createElement('p');
+      temperature.textContent = "Temperature: " + item.main.temp + "°F";
+
+      var description = document.createElement('p');
+      description.textContent = "Description: " + item.weather[0].description;
+
+      // Append the elements to the weatherSection div
+      weatherSection.appendChild(img);
+      weatherSection.appendChild(date);
+      weatherSection.appendChild(cityName);
+      weatherSection.appendChild(temperature);
+      weatherSection.appendChild(description);
+
+
+      // Append the weatherSection div to the resultContainer
+
+      resultContainer.appendChild(weatherSection);
+
+    }
+
+  });
+}
+
+
+
+//   // Loop through the weather list and append each item to the result container
+
+//   weatherList.forEach(function(item) {
+//     var weatherItem = document.createElement('p');
+//     weatherItem.textContent = JSON.stringify(item);
+//     resultContainer.appendChild(weatherItem);
+
+
+
+//     console.log(data);
 //   });
-// };
-
-// fetchButton.addEventListener('click', getApi);
+// }
 
 
-// console.log(zipCode);
 
-// fetch(queryURL);
+fetchButton.addEventListener('click', textValue);
 
-// console.log(queryURL)
 
