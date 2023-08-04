ef755b6dfe9025cecf4d83cd3c91be0aa79f6862
mbman11 <m.behrmann11@gmail.com>
2023-08-04 16:17:30 -0600

    added descriptions to weather, added styling, added headings, added local storage

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
 var apiKey = "656a0c48d465cbcb32447829872dc37e";
-
-var fetchButton = document.getElementById('button');
-
+var fetchButton = document.getElementById('button-1');
 var userInput = document.getElementById('user-input').value;
-
 
 
 function textValue() {
   var userInput = document.getElementById('user-input').value;
 
+  if(userInput.length !== 5 || userInput == NaN) {
+    var wrongInputP = document.createElement('p');
+    wrongInputP.textContent = "Please Enter A Correct ZipCode.";
+    var idLeftDiv = document.getElementById('left-side-div');
+    idLeftDiv.append(wrongInputP);
+    return;
+  } if (userInput.length == 5 && !NaN) {
+    var wrongInputP = document.createElement('p');
+  } 
+
   var idLeftDiv = document.getElementById('left-side-div');
 
-  var createUserInEl = document.createElement('h4');
+  var createUserInEl = document.createElement('button');
   createUserInEl.textContent = userInput;
-
   idLeftDiv.append(createUserInEl);
 
 
@@ -21,7 +27,11 @@ function textValue() {
   var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?zip" + userInput + "&appid=" + apiKey + "&units=imperial";
   currentWeather(queryURL2 , queryURL1)
   
-  
+  function displayLocalStorage(){
+    localStorage.setItem('value', userInput);
+    userInput.innerHTML = localStorage.getItem('value');
+  }
+  displayLocalStorage();
 }
 
 function currentWeather(queryURL2, queryURL1) {
@@ -30,8 +40,9 @@ function currentWeather(queryURL2, queryURL1) {
   }).then(function (data) {
 
     var  mainResultC = document.getElementById('result')
+
     var cWeatherArea = document.getElementById('current-weather');
-    var cWeatherContent = data.city.name + ', ' + data.city.country + ' ' + data.list[0].dt_txt.split(" ")[0];
+    var cWeatherContent ="Today's Weather: " + data.city.name + ', ' + data.city.country + ' ' + data.list[0].dt_txt.split(" ")[0];
 
     var temperature = document.createElement('p')
     temperature.textContent = 'Temp: ' + data.list[0].main.temp + '°F';
@@ -41,6 +52,9 @@ function currentWeather(queryURL2, queryURL1) {
 
     var humidity = document.createElement('p')
     humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + ' MPH';
+
+    var description = document.createElement('p');
+    description.textContent = "Description: " + data.list[0].weather[0].description;
 
 
     var cWeatherInfoEl = document.createElement('h2');
@@ -55,6 +69,7 @@ function currentWeather(queryURL2, queryURL1) {
     cWeatherArea.append(temperature);
     cWeatherArea.append(wind);
     cWeatherArea.append(humidity);
+    cWeatherArea.append(description);
 
     mainResultC.append(cWeatherArea);
 
@@ -90,38 +105,52 @@ function handleApiResponse(data) {
   //reset container
   resultContainer.innerHTML = "";
 
+  var fiveDayTitle = document.createElement('div');
+  fiveDayTitle.setAttribute('id','five-day-title');
+  fiveDayTitle.querySelector("#five-day-title");
+  fiveDayTitle.textContent = fiveDayP;
+  var fiveDayP = document.createElement('p');
+  fiveDayP.textContent = "5 Day Forecast: ";
+
+  fiveDayTitle.append(fiveDayP);
+  
+  resultContainer.append(fiveDayTitle);
 
   weatherList.forEach(function (item) {
     var time = item.dt_txt.split(" ")[1]
     if (time === "12:00:00") {
-      // Create a new div element for each weather forecast object
       var weatherSection = document.createElement('div');
-      weatherSection.classList.add('weather-section'); // Add a CSS class for styling (optional)
-      var img = document.createElement("img");
-      img.setAttribute("src",`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`)
+      weatherSection.classList.add('weather-section');
 
-      // Create and populate elements with the relevant weather forecast data
-      var cityName = document.createElement('h3')
-      cityName.textContent = data.city.name;
+      // var cityName = document.createElement('h3')
+      // cityName.textContent = data.city.name;
 
       var date = document.createElement('p');
       date.textContent = "Date: " + item.dt_txt.split(" ")[0];
 
+      var img = document.createElement("img");
+      img.setAttribute("src",`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`)
+
       var temperature = document.createElement('p');
       temperature.textContent = "Temperature: " + item.main.temp + "°F";
+
+      var wind = document.createElement('p')
+      wind.textContent = 'Wind Speed: ' + data.list[0].wind.speed + ' MPH';
+
+      var humidity = document.createElement('p')
+      humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
 
       var description = document.createElement('p');
       description.textContent = "Description: " + item.weather[0].description;
 
-      // Append the elements to the weatherSection div
-      weatherSection.appendChild(img);
+  
       weatherSection.appendChild(date);
-      weatherSection.appendChild(cityName);
+      weatherSection.appendChild(img);
       weatherSection.appendChild(temperature);
+      weatherSection.appendChild(wind);
+      weatherSection.appendChild(humidity);
       weatherSection.appendChild(description);
 
-
-      // Append the weatherSection div to the resultContainer
 
       resultContainer.appendChild(weatherSection);
 
